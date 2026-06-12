a115897d8d0057c33c6364198be8329df3cae6d7
Baistes1 <lepandachrist81@gmail.com>
2026-06-13 00:39:20 +0100

    Last The think

---
+++ src/components/Elements.js
@@ -0,0 +1,26 @@
+export const Navbar = [ "Contact Us","Home", "About Us"]
+
+export const Footer = ["About Us", "Contact Us", "Terms of Service", "Privacy Policy"]
+
+export const SocialMedia = ["TikTok", "Instagram", "Whatsapp"]
+
+export const Products = [
+    {
+        id: 1,
+        name: "Heritage Classic",
+        price: 130,
+        color: ["Blanc", "Noir", "Vert"],
+        images: {
+            homme: {
+                "Blanc": "img/Men_White.png",
+                "Noir":  "img/Man_Black.png",
+                "Vert":  "img/Man_Green.png"
+            },
+            femme: {
+                "Blanc": "img/Girl_White.png",
+                "Noir":  "img/Girl_Black.png",
+                "Vert":  "img/Girl_Green.png"
+            }
+        }
+    }
+]
